0705bb3c6258112f55232bc27b692a9902613b6a
SanyamB9187 <sanyambains7@gmail.com>
2024-09-18 22:42:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,41 +2,41 @@ var randomVar = Math.floor(Math.random()*6)+1;
 var randomVar2 = Math.floor(Math.random()*6)+1;
  
     if(randomVar===6){
-        document.querySelector("img").setAttribute("src", "images/dice6.png");
+        document.querySelector("img").setAttribute("src", "dice6.png");
     }
     else if(randomVar===5){
-        document.querySelector("img").setAttribute("src", "images/dice5.png");
+        document.querySelector("img").setAttribute("src", "dice5.png");
     }
     else if(randomVar===4){
-        document.querySelector("img").setAttribute("src", "images/dice4.png");
+        document.querySelector("img").setAttribute("src", "dice4.png");
     }
     else if(randomVar===3){
-        document.querySelector("img").setAttribute("src", "images/dice3.png");
+        document.querySelector("img").setAttribute("src", "dice3.png");
     }
     else if(randomVar===2){
-        document.querySelector("img").setAttribute("src", "images/dice2.png");
+        document.querySelector("img").setAttribute("src", "dice2.png");
     }
     else if(randomVar===1){
-        document.querySelector("img").setAttribute("src", "images/dice1.png");
+        document.querySelector("img").setAttribute("src", "dice1.png");
     }
 
     if(randomVar2===6){
-        document.querySelectorAll("img")[1].setAttribute("src", "images/dice6.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "dice6.png");
     }
     else if(randomVar2===5){
-        document.querySelectorAll("img")[1].setAttribute("src", "images/dice5.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "dice5.png");
     }
     else if(randomVar2===4){
-        document.querySelectorAll("img")[1].setAttribute("src", "images/dice4.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "dice4.png");
     }
     else if(randomVar2===3){
-        document.querySelectorAll("img")[1].setAttribute("src", "images/dice3.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "dice3.png");
     }
     else if(randomVar2===2){
-        document.querySelectorAll("img")[1].setAttribute("src", "images/dice2.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "dice2.png");
     }
     else if(randomVar2===1){
-        document.querySelectorAll("img")[1].setAttribute("src", "images/dice1.png");
+        document.querySelectorAll("img")[1].setAttribute("src", "dice1.png");
     }
 
     if(randomVar> randomVar2){
